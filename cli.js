6e72a5c1e5a9ebbb1e408930eb9c261cc1e80dd0
Gilles De Mey <gilles.de.mey@gmail.com>
2016-01-01 20:52:56 +0100

    Extended CLI usage

--- cli.js
+++ cli.js
@@ -1,12 +1,27 @@
 #!/usr/bin/env node
-var path = require('path')
+var resolve = require('path').resolve
+var meow = require('meow')
 
 var initdify = require('./')
-var pjson = require(path.resolve(process.cwd(), './package.json'))
+
+var cli = meow(`
+  Usage
+    $ systemdify <folder>
+
+  Examples
+    $ systemdify /path/to/my/app
+
+    [Unit]
+    description=My Application
+    ...
+`)
+
+var folder = cli.input[0] || process.cwd()
+var pjson = require(resolve(folder, 'package.json'))
 
 var file = initdify({
   name: pjson.name,
-  command: pjson.main,
+  command: resolve(folder, pjson.main),
   description: pjson.description
 })
 
